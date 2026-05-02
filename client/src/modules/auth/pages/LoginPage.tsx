20df1de3c46f2cd62e4d8a1abe50ec4d43e40629
Yana Ovcharik <yanaovcharik@gmail.com>
2026-05-02 13:12:13 +0300

    feat(server): add max-width for LoginForm

--- client/src/modules/auth/pages/LoginPage.tsx
+++ client/src/modules/auth/pages/LoginPage.tsx
@@ -1,11 +1,15 @@
 import { CenteredLayout, FormCard } from '@/common/components';
 import { LoginForm } from '../components/LoginForm';
 import Title from 'antd/es/typography/Title';
+import styles from './LoginPage.module.scss';
 
 export const LoginPage = () => {
   return (
     <CenteredLayout>
-      <FormCard title={<Title level={4}>Login</Title>}>
+      <FormCard
+        title={<Title level={4}>Login</Title>}
+        className={styles.formLogin}
+      >
         <LoginForm />
       </FormCard>
     </CenteredLayout>
