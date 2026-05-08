3531fedfb8619c8dd615b1be78f9677078ccb1b5
Yana Ovcharik <yanaovcharik@gmail.com>
2026-05-08 17:51:41 +0300

    feat(client): add button "Back to all cities" at CityDetailsPage

--- client/src/modules/weatherDetails/pages/CityDetailsPage.tsx
+++ client/src/modules/weatherDetails/pages/CityDetailsPage.tsx
@@ -1,13 +1,19 @@
-import { Spin, Space, Alert } from 'antd';
-import { PageLayout, Header } from '@/common/components';
+import { Spin, Space, Alert, Button } from 'antd';
+import { PageLayout, Header, FormCard } from '@/common/components';
 
 import { useCityWeather } from '../hooks/useCityWeather';
 import { CurrentWeatherCard } from '../components/CurrentWeatherCard/CurrentWeatherCard';
 import { HourlyForecast } from '../components/HourlyForecast/HourlyForecast';
 import { DailyForecast } from '../components/DailyForecast/DailyForecast';
+import { useNavigate } from 'react-router';
 
 export const CityDetailsPage = () => {
   const { city, weather, loading, error } = useCityWeather();
+  const navigate = useNavigate();
+
+  const handleBack = () => {
+    navigate('/');
+  };
 
   if (error) {
     return (
@@ -28,6 +34,11 @@ export const CityDetailsPage = () => {
   return (
     <PageLayout header={<Header />}>
       <Space orientation="vertical" size="large" style={{ width: '100%' }}>
+        <FormCard>
+          <Button type="default" onClick={handleBack}>
+            ← Back to all cities
+          </Button>
+        </FormCard>
         <CurrentWeatherCard city={city} weather={weather} />
         <HourlyForecast hourly={weather.hourly} />
         <DailyForecast daily={weather.daily} />
